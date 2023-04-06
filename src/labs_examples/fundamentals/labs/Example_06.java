871fd5056f4bb229f9886059885049409f28f983
Zane Stickel <zanemstickel@gmail.com>
2023-04-05 21:27:08 -0400

    added first file in intelij

---
+++ src/labs_examples/fundamentals/labs/Example_06.java
@@ -0,0 +1,12 @@
+package labs_examples.fundamentals.labs;
+
+public class Example_06 {
+    public static void main(String[] args) {
+        System.out.println("First new class");
+        System.out.println(multiply(5, 5));
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -15,9 +15,12 @@ public class Exercise_03 {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
-
+        System.out.println("Yet another hello world!");
         // write code to print out "Check out my first program!" below
-
+        int i = 10;
+        int x = 12;
+        int z = i + x;
+        System.out.println(z);
     }
 
 }
